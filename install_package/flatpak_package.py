455086ce9d683f4c7144edc2aaa68cf5148706db
lens0021 <lorentz0021@gmail.com>
2023-06-06 07:40:34 +0900

    fix the name of flatpak package

--- install_package/flatpak_package.py
+++ install_package/flatpak_package.py
@@ -4,7 +4,7 @@ import shell_command
 class FlatpakPackage(Package):
 
   def is_installed(self):
-    return shell_command.exec('flatpak list --app | grep com.authy.Authy; echo $?') == '0'
+    return shell_command.exec(f'flatpak list --app | grep ${self.NAME}; echo $?') == '0'
 
   def real_install(self):
     shell_command.exec('flatpak install -y ' + self.get_name())
