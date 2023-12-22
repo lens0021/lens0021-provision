f58085cda6970eb42d4692c9122d38b0801ec74d
lens0021 <lorentz0021@gmail.com>
2023-12-22 23:39:57 +0900

    Add symlinks.fish

---
+++ config/fish/conf.d/symlinks.fish
@@ -0,0 +1,15 @@
+function create-symlink
+  set -l src (realpath $argv[1])
+  set -l dist $argv[2]
+
+  if test ! -f $dist
+    mkdir -p (dirname $dist)
+    ln -s $src $dist
+  end
+end
+
+set -l pr_dir $HOME/git/lens/provision
+
+create-symlink $pr_dir/config/tmux.conf $HOME/.config/tmux/tmux.conf
+create-symlink $pr_dir/config/helix/config.toml $HOME/.config/helix/config.toml
+
