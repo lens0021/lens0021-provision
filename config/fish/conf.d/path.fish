d6c169b195603096833e4203014ad68917cf077a
lens0021 <lorentz0021@gmail.com>
2023-12-22 22:11:54 +0900

    Add go path to PATH# Please enter the commit message for your changes. Lines starting

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -1,5 +1,6 @@
 if status is-login
   set -gx PATH $PATH $HOME/.local/bin
+  set -gx PATH $PATH $HOME/go/bin
   set -gx PATH $PATH $HOME/bin
   if set -q KREW_ROOT
     set -gx PATH $PATH $KREW_ROOT/bin
