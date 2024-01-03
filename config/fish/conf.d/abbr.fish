7042868bd6b08131a7dca61e63871d70eb03394c
lens0021 <lorentz0021@gmail.com>
2024-01-03 15:02:11 +0900

    fish: abbr for git log --graph

--- config/fish/conf.d/abbr.fish
+++ config/fish/conf.d/abbr.fish
@@ -3,4 +3,6 @@ if status is-interactive
   abbr -a -- pbcopy 'xclip -selection clipboard'
   abbr -a -- pbpaste 'xclip -selection clipboard -o'
   abbr -a -- kc kubectl
+
+  abbr -a -- gitgraph 'git log --graph --all --decorate --oneline --color'
 end
