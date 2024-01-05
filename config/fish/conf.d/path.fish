54bf0ef8898917e77f82e93e6a16a873e2141775
lens0021 <lorentz0021@gmail.com>
2024-01-05 23:55:14 +0900

    fish: Add krew to PATH anyway

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -4,6 +4,7 @@ if status is-login
   set -gx PATH $PATH $HOME/bin
   if set -q KREW_ROOT
     set -gx PATH $PATH $KREW_ROOT/bin
+  else if test -d $HOME/.krew/bin
     set -gx PATH $PATH $HOME/.krew/bin
   end
 end
