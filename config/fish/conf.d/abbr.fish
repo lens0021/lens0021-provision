e3589697bc2d648649df440057aa78752e5d1e3f
lens0021 <lorentz0021@gmail.com>
2024-01-20 10:55:21 +0900

    Add abbrs

--- config/fish/conf.d/abbr.fish
+++ config/fish/conf.d/abbr.fish
@@ -1,8 +1,11 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
+
   abbr -a -- pbcopy 'xclip -selection clipboard'
   abbr -a -- pbpaste 'xclip -selection clipboard -o'
   abbr -a -- kc kubectl
+  abbr -a -- tf terraform
+  abbr -a -- cd-fzf 'cd (rg --files | fzf --preview \'ls {}\')'
 
   abbr -a -- gitgraph 'git log --graph --all --decorate --oneline --color'
 end
