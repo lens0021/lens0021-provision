3d171dd28496669f741301376f8ad839709de2f2
lens0021 <lorentz0021@gmail.com>
2024-01-20 11:15:17 +0900

    Remove cd-fzf abbr

--- config/fish/conf.d/abbr.fish
+++ config/fish/conf.d/abbr.fish
@@ -5,7 +5,6 @@ if status is-interactive
   abbr -a -- pbpaste 'xclip -selection clipboard -o'
   abbr -a -- kc kubectl
   abbr -a -- tf terraform
-  abbr -a -- cd-fzf 'cd (rg --files | fzf --preview \'ls {}\')'
 
   abbr -a -- gitgraph 'git log --graph --all --decorate --oneline --color'
 end
