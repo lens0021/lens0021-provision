da1ffeb8d7b0b78af70ff29db6885d70cd449ee2
lens0021 <lorentz0021@gmail.com>
2024-02-03 19:19:44 +0900

    Bump php dependencies

--- config/fish/conf.d/path.fish
+++ config/fish/conf.d/path.fish
@@ -1,6 +1,7 @@
 if status is-login
   set -gx PATH $PATH $HOME/.local/bin
   set -gx PATH $PATH $HOME/go/bin
+  set -gx PATH $PATH $HOME/.composer/vendor/bin
   set -gx PATH $PATH $HOME/bin
   if set -q KREW_ROOT
     set -gx PATH $PATH $KREW_ROOT/bin
