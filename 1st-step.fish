a3099eb743595505712d424e7088189ce75360c7
lens0021 <lorentz0021@gmail.com>
2024-05-01 10:55:27 +0900

    termux: Add user

--- 1st-step.fish
+++ 1st-step.fish
@@ -1,7 +1,34 @@
+if ! string match --regex --quiet nemo "$(cat /etc/passwd)"
+  adduser nemo
+  passwd -d nemo
+end
+
+if ! -e /etc/sudeors.d
+  echo 'nemo ALL=(ALL:ALL) ALL' > /etc/sudeors.d/nemo
+end
+
+if set -q TERMUX_VERSION
+  # Termux X11 Desktop
+  dnf install -y \
+    dbus-x11 \
+    gnome-shell \
+    gnome-software \
+    gnome-tweaks \
+    nautilus \
+    python-dbus \
+  ;
+
+  bash -c 'for file in $(find /usr -type f -iname "*login1*"); do rm -rf $file; done'
+end
+
+if ! command -v fisher >/dev/null
+  fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher'
+end
+
 if [ ! -d /usr/local/git ]
   mkdir -p /usr/local/git
-  chown $USER:(id -g) git
-  cd git
+  chown nemo:nemo /usr/local/git
+  cd /usr/local/git
   sudo mkdir \
     lens \
     fw \
